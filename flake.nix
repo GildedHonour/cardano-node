dbf4a33f5f0153e74110506592e2397571635b2d
Michael Fellinger <michael.fellinger@iohk.io>
2021-01-20 17:10:08 +0100

    nix flake

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Cardano Node";
+
+  inputs = {
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, haskell-nix, ... }:
+    (utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        legacyPackages = import ./nix {
+          inherit system;
+          ownHaskellNix = haskell-nix.legacyPackages.${system};
+          gitrev = self.rev or "dirty";
+        };
+
+        lib = nixpkgs.lib;
+        sources = import ./nix/sources.nix { };
+        iohkNix = import sources.iohk-nix { inherit system; };
+        environments = iohkNix.cardanoLib.environments;
+        environmentName = "testnet";
+
+        eachEnv = lib.flip lib.pipe [
+          (lib.forEach (builtins.attrNames environments))
+          lib.listToAttrs
+        ];
+
+        config = env:
+          { pkgs, ... }: {
+            services.cardano-node = rec {
+              enable = true;
+              package = pkgs.cardano-node;
+              environment = env;
+              cardanoNodePkgs = pkgs;
+              stateDir = "/local";
+              hostAddr = "0.0.0.0";
+              port = 3001;
+            };
+          };
+
+        evaluated = env:
+          lib.nixosSystem {
+            inherit system;
+            pkgs = legacyPackages;
+            modules = [ ./nix/nixos/cardano-node-service.nix (config env) ];
+          };
+
+        packages = eachEnv (env:
+          lib.nameValuePair "cardano-node-${env}"
+          (legacyPackages.writeShellScriptBin "cardano-node-entrypoint" ''
+            ${(evaluated env).config.services.cardano-node.script}
+          ''));
+      in {
+        inherit environments evaluated legacyPackages packages;
+
+        apps = eachEnv (env:
+          lib.nameValuePair "cardano-node-${env}" (utils.lib.mkApp {
+            drv = packages."cardano-node-${env}";
+            exePath = "/bin/cardano-node-entrypoint";
+          }));
+      }));
+}
